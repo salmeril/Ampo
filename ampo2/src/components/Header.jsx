9eb2339cc9cbff6256433b2a5e47177d1c728c65
salmeril <54689767+salmeril@users.noreply.github.com>
2024-03-10 19:19:03 -0300

    1

---
+++ ampo2/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="flex items-center w-full p-4">
+        <div className="bg-red-700 w-1/6">
+            logo
+        </div>
+        <nav className="bg-blue-600 flex-1">
+            Navega
+        </nav>
+    </header>
+  )
+}
+
+export default Header
